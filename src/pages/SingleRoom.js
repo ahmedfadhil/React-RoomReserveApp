c1fd032f38c8af96745f17bed1bc0c1fa7ccb8bf
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2020-01-22 23:02:08 +0100

    added rooms component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react'
-
+import defaultBcg from '../images/room-1.jpeg'
+import Hero from '../components/Hero'
+import moduleName from '../components/Banner'
+import { Link } from 'react-router-dom'
+import { RoomContext } from '../Context'
 export default class SingleRoom extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg
+        }
+    }
+    // componentDidMount() { }
+    static contextType = RoomContext
     render() {
+        const { getRoom } = this.contextType
         return (
             <div>
                 <h3>hello from single room</h3>
